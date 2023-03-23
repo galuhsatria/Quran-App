73b5e48509c602fbdafde9281a7c67fd4e807bad
Galuh satria <96463812+galuhsatria@users.noreply.github.com>
2023-03-23 12:15:14 +0700

    Update Header.jsx
    change images

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -6,7 +6,7 @@ const Header = () => {
   return (
     <div className="bg-primary pt-8 pb-2 mb-4 mt-3 rounded-lg px-4 mx-3" id="header">
       <div>
-        <img src="../../public/image/alquran-logo.png" alt="" className="m-auto w-64 z-10" />
+        <img src="https://i.ibb.co/vQkH8JM/alquran-logo.png" alt="" className="m-auto w-64 z-10" />
         <Search />
         <div className="flex gap-2 justify-center pb-4">
         <Link to={`/surah/36`} className="bg-white font-semibold px-3  py-2 rounded-full">Yasin</Link>
