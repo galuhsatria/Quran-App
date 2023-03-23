378bf6bd38197394f2576aed21301efaf443fdaa
Galuh satria <96463812+galuhsatria@users.noreply.github.com>
2023-03-23 14:04:03 +0800

    Update DetailSurah.jsx
    Update

--- src/pages/DetailSurah/DetailSurah.jsx
+++ src/pages/DetailSurah/DetailSurah.jsx
@@ -33,7 +33,7 @@ const DetailSurah = () => {
         <div className="ayat bg-white px-4 rounded-lg flex flex-col gap-3 py-5 border-2 border-primary" key={index}>
           <div className="relative w-10  flex justify-center items-center">
             <img src="https://i.ibb.co/RC0C8r2/number.png" alt="" className="absolute" />
-            <p className="text-sm">{nomor}</p>
+            <p className="text-sm">{ayat.nomor}</p>
           </div>
           <div id="ayat" className="text-end">
             <h3 className="font-arabic text-2xl">{ayat.ar}</h3>
